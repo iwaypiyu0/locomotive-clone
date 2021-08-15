ec21eff72c9b12eb6060baec08385fd927798652
iwaypiyu0 <iwaypiyu@gmail.com>
2021-08-15 21:31:13 +0800

    divider animation fix

--- main.js
+++ main.js
@@ -26,6 +26,18 @@ gsap.fromTo(document.querySelector('#key-visual-section .right-title'), {
   },
 });
 
+gsap.fromTo(Array.from(document.querySelectorAll('#key-visual-section .title-1 , #key-visual-section .title-2')), {
+  transform: 'translateY(0)',
+}, {
+  transform: (index) => index ? 'translateY(-4vh)' : 'translateY(-9vh)',
+  scrollTrigger: {
+    start: 'top top',
+    end: '+=100vh',
+    scrub: true,
+  },
+});
+
+
 const splitTextElements = document.querySelectorAll('#key-visual-section .split-version > span');
 
  gsap.to(Array.from(splitTextElements), {
@@ -48,3 +60,15 @@ const splitTextElements = document.querySelectorAll('#key-visual-section .split-
    },
  });
 
+gsap.fromTo(document.querySelector('#key-visual-section .divider'), {
+  scaleX: 0,
+}, {
+  scaleX: 1,
+  scrollTrigger: {
+    trigger: '#key-visual-section .divider',
+    toggleActions: 'restart pause reverse pause',
+    start: '20px 85%',
+    end: 'bottom bottom',
+    scrub: 1,
+  },
+});
